bf12342965a11846767322b03e254a5d2d19e4d6
Steve Losh <steve@stevelosh.com>
2010-01-21 09:14:02 -0500

    Add the d alias.

--- zsh/tasks.zsh
+++ zsh/tasks.zsh
@@ -5,6 +5,7 @@ alias m='~/lib/t/t.py --task-dir="~/Dropbox/tasks" --list=music.txt'
 alias g='~/lib/t/t.py --task-dir="~/Dropbox/tasks" --list=groceries.txt'
 alias k='~/lib/t/t.py --task-dir="~/Dropbox/tasks" --list=books.txt'
 alias p='~/lib/t/t.py --task-dir="~/Dropbox/tasks" --list=pack.txt'
+alias d='~/lib/t/t.py --task-dir="~/Desktop" --list=todo.txt --delete-if-empty'
 alias b='~/lib/t/t.py --list=bugs'
 
 alias pa='~/lib/t/t.py --task-dir="~/Dropbox/tasks" --list=pack-archive'
