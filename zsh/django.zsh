e86e8252683ba6925f554211b077971569038956
Steve Losh <steve@stevelosh.com>
2010-01-28 09:06:48 -0500

    Add a pmrpg alias.

--- zsh/django.zsh
+++ zsh/django.zsh
@@ -1,4 +1,5 @@
 alias pmr='python manage.py runserver'
 alias pmrp='python manage.py runserver_plus'
+alias pmrpg='pmrp --adminmedia=`pwd`/media/admin'
 alias pmsdb='python manage.py syncdb'
 alias pms='python manage.py shell'
