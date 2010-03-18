c12cbc630c32cf43a9f897d9649cb073a517a66d
Steve Losh <steve@stevelosh.com>
2010-03-18 15:45:28 -0400

    Move the django.zsh file to oh-my-zsh.

--- zsh/django.zsh
+++
@@ -1,5 +0,0 @@
-alias pmr='python manage.py runserver'
-alias pmrp='python manage.py runserver_plus'
-alias pmrpg='pmrp --adminmedia=`pwd`/media/admin'
-alias pmsdb='python manage.py syncdb'
-alias pms='python manage.py shell'
