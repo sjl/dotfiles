2d308e9f020080697f5bfa2f8f139dd09263e3e6
Steve Losh <steve@stevelosh.com>
2010-06-16 21:00:20 -0400

    Fixes to bootstrap.

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,7 @@
 # BE CAREFUL, DAMMIT
 
 # Don't forget the SSH keys.
+# Setuptools needs to be installed.
 
 mkdir -p lib/hg
 mkdir -p lib/python
@@ -31,27 +32,32 @@ hg clone http://bitbucket.org/sjl/dotfiles ~/lib/dotfiles
 git clone git://github.com/sjl/oh-my-zsh ~/lib/oh-my-zsh
 git clone git://github.com/sjl/z-zsh ~/lib/z
 
+hg clone http://bitbucket.org/ianb/pip/ ~/lib/python/pip
+cd ~/lib/python/pip
+sudo python setup.py install
+cd
+
+sudo pip install virtualenv
+sudo pip install rope
+
 hg clone http://bitbucket.org/dhellmann/virtualenvwrapper ~/lib/python/virtualenvwrapper
 cd ~/lib/python/virtualenvwrapper
-sudo python setup.py develop
+sudo python setup.py install
 cd
 
-hg clone http://bitbucket.org/agr/rope ~/lib/python/rope
-cd ~/lib/python/rope
-sudo python setup.py develop
-cd
+rm -rf ~/lib/oh-my-zsh/custom
+ln -s "$HOME/lib/dotfiles/zsh" "$HOME/lib/oh-my-zsh/custom"
 
-rm -rf ~lib/oh-my-zsh/custom
-ln -s "$HOME/lib/dotfiles/zsh $HOME/lib/oh-my-zsh/custom"
-
-ln -s "$HOME/lib/dotfiles/.ackrc $HOME/.ackrc"
-ln -s "$HOME/lib/dotfiles/.gitconfig $HOME/.gitconfig"
-ln -s "$HOME/lib/dotfiles/.hgrc $HOME/.hgrc"
-ln -s "$HOME/lib/dotfiles/vim/.vim $HOME/.vim"
-ln -s "$HOME/lib/dotfiles/vim/.vimrc $HOME/.vimrc"
+ln -s "$HOME/lib/dotfiles/.ackrc" "$HOME/.ackrc"
+ln -s "$HOME/lib/dotfiles/.gitconfig" "$HOME/.gitconfig"
+ln -s "$HOME/lib/dotfiles/.hgrc" "$HOME/.hgrc"
+ln -s "$HOME/lib/dotfiles/vim/.vim" "$HOME/.vim"
+ln -s "$HOME/lib/dotfiles/vim/.vimrc" "$HOME/.vimrc"
 
 rm ~/.zshrc
-ln -s "$HOME/lib/dotfiles/.zshrc $HOME/.zshrc"
+rm ~/.bashrc
+rm ~/.bash_profile
+ln -s "$HOME/lib/dotfiles/.zshrc" "$HOME/.zshrc"
 
 hg clone bb://sjl/hg-prompt/ "$HOME/lib/hg/hg-prompt"
 hg clone bb://sjl/hg-paste/ "$HOME/lib/hg/hg-paste"
@@ -60,6 +66,7 @@ hg clone bb://ccaughie/hgcollapse/ "$HOME/lib/hg/hgcollapse"
 hg clone bb://tksoh/hgshelve/ "$HOME/lib/hg/hgshelve"
 hg clone bb://durin42/histedit/ "$HOME/lib/hg/histedit"
 hg clone bb://durin42/hg-git/ "$HOME/lib/hg/hg-git"
+hg clone bb://Bill_Barry/hgattic "$HOME/lib/hg/hgattic"
 
 git clone git://github.com/jelmer/dulwich.git "$HOME/lib/dulwich"
-ln -s "$HOME/lib/dulwich/dulwich $HOME/lib/hg/hg-stable/dulwich"
+ln -s "$HOME/lib/dulwich/dulwich" "$HOME/lib/hg/hg-stable/dulwich"
