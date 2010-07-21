e71f7ed4caf2dcb329472989622933112fd9f121
Steve Losh <steve@dwaiter.com>
2010-07-21 16:33:47 -0400

    zsh: remove unused aliases

--- zsh/mercurial.zsh
+++ zsh/mercurial.zsh
@@ -1,8 +1,5 @@
 #!/usr/bin/env zsh
 
-alias calctime="sed -e 's/{t: *\([0-9]*\)*.*/\1/' | python -c 'import sys; print sum(map(int, sys.stdin.readlines())) / 60.0, \"hours\"'"
-alias hgt='hg log -vd "`date -j \"+%Y-%m-%d\"`" -u steve | grep "{t:" | calctime'
-
 function pull_everything() {
     for repo in $( ls -1 ); do
         if [[ -d $repo && -d $repo/.hg ]]; then
