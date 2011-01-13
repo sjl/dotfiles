e9b5d5a6daa429bcb3429346f984dce291106df0
Steve Losh <steve@stevelosh.com>
2011-01-13 12:40:43 -0500

    Lots.

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -13,32 +13,33 @@ alias c       'clear'
 alias M       'mvim .'
 
 # Environment variables ------------------------------------------------------
-set -g EDITOR vim
-set -g PATH "$HOME/.gem/ruby/1.8/bin:$PATH"
-set -g PATH "/usr/local/bin:/usr/local/sbin:$HOME/bin:/opt/local/bin:$PATH"
-set -g PATH "/opt/subversion/bin:$PATH"
-set -g COMMAND_MODE unix2003
-set -g RUBYOPT rubygems
-set -g CLASSPATH "$CLASSPATH:/usr/local/Cellar/clojure-contrib/1.2.0/clojure-contrib.jar"
+set PATH "$HOME/.gem/ruby/1.8/bin:$PATH"
+set PATH "/usr/local/bin:/usr/local/sbin:$HOME/bin:/opt/local/bin:$PATH"
+set PATH "/opt/subversion/bin:$PATH"
+set -g -x EDITOR vim
+set -g -x COMMAND_MODE unix2003
+set -g -x RUBYOPT rubygems
+set -g -x CLASSPATH "$CLASSPATH:/usr/local/Cellar/clojure-contrib/1.2.0/clojure-contrib.jar"
 
 # Python variables -----------------------------------------------------------
-set -g PIP_DOWNLOAD_CACHE "$HOME/.pip/cache"
-set -g PYTHONSTARTUP "$HOME/.pythonrc.py"
-set -g WORKON_HOME "$HOME/lib/virtualenvs"
+set -g -x PIP_DOWNLOAD_CACHE "$HOME/.pip/cache"
+set -g -x PYTHONSTARTUP "$HOME/.pythonrc.py"
+set -g -x WORKON_HOME "$HOME/lib/virtualenvs"
 
-set -g PATH "$PATH:/usr/local/Cellar/PyPi/3.6/bin"
-set -g PATH "$PATH:/usr/local/Cellar/python/2.7.1/bin"
-set -g PATH "$PATH:/usr/local/Cellar/python/2.7/bin"
-set -g PATH "$PATH:/usr/local/Cellar/python/2.6.5/bin"
+set PATH "$PATH:/usr/local/Cellar/PyPi/3.6/bin"
+set PATH "$PATH:/usr/local/Cellar/python/2.7.1/bin"
+set PATH "$PATH:/usr/local/Cellar/python/2.7/bin"
+set PATH "$PATH:/usr/local/Cellar/python/2.6.5/bin"
 
-set -g PYTHONPATH "$PYTHONPATH:/usr/local/lib/python2.7.1/site-packages"
-set -g PYTHONPATH "$PYTHONPATH:/usr/local/lib/python2.7/site-packages"
-set -g PYTHONPATH "$PYTHONPATH:/usr/local/lib/python2.6/site-packages"
-set -g PYTHONPATH "$HOME/lib/python/see:$PYTHONPATH"
+set -g -x PYTHONPATH ""
+set PYTHONPATH "$PYTHONPATH:/usr/local/lib/python2.7.1/site-packages"
+set PYTHONPATH "$PYTHONPATH:/usr/local/lib/python2.7/site-packages"
+set PYTHONPATH "$PYTHONPATH:/usr/local/lib/python2.6/site-packages"
+set PYTHONPATH "$HOME/lib/python/see:$PYTHONPATH"
 
 # Mercurial variables --------------------------------------------------------
-set -g PATH="$HOME/lib/hg/hg-stable:$PATH"
-set -g PYTHONPATH="$HOME/lib/hg/hg-stable:$PYTHONPATH"
+set PATH "$HOME/lib/hg/hg-stable:$PATH"
+set PYTHONPATH "$HOME/lib/hg/hg-stable:$PYTHONPATH"
 
 # Extra ----------------------------------------------------------------------
 . ~/src/z-fish/z.fish
@@ -50,4 +51,10 @@ end
 
 function z_add --on-event prompt
     z --add "$PWD"
+end
+
+# hgd (for now) --------------------------------------------------------------
+
+function h
+    /Users/sjl/src/hgd/hd $argv
 end
