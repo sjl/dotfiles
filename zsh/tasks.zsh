ba019c927e46729014ad7eb8d849ad834a20ecfb
Steve Losh <steve@stevelosh.com>
2011-06-21 14:55:21 -0400

    More cleanup.

--- zsh/tasks.zsh
+++ zsh/tasks.zsh
@@ -1,11 +1,10 @@
 #!/usr/bin/env zsh
 
 alias t='~/lib/t/t.py --task-dir="~/Dropbox/tasks" --list=tasks.txt'
-alias w='~/lib/t/t.py --task-dir="~/Dropbox/tasks" --list=work.txt'
-alias p='~/lib/t/t.py --task-dir="~/Dropbox/tasks" --list=pack.txt'
-alias b='~/lib/t/t.py --list=bugs'
 
+alias p='~/lib/t/t.py --task-dir="~/Dropbox/tasks" --list=pack.txt'
 alias pa='~/lib/t/t.py --task-dir="~/Dropbox/tasks" --list=pack-archive'
+
 function packfor() {
     cp "$HOME/Dropbox/tasks/pack-archive" "$HOME/Dropbox/tasks/pack.txt"
     touch "$HOME/Dropbox/tasks/.pack.txt.done"
