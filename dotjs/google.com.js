44aea0ee09d7ee964cc02006b3eabdeb563d3298
Steve Losh <steve@stevelosh.com>
2011-07-12 17:00:13 -0400

    Dear God can we lose the stupid previews already?

---
+++ dotjs/google.com.js
@@ -0,0 +1,18 @@
+$(function() {
+    var css = ".vspib, .vspi {display: none;} #vspb {display: none;}";
+    if (typeof GM_addStyle != "undefined") {
+        GM_addStyle(css);
+    } else if (typeof PRO_addStyle != "undefined") {
+        PRO_addStyle(css);
+    } else if (typeof addStyle != "undefined") {
+        addStyle(css);
+    } else {
+        var heads = document.getElementsByTagName("head");
+        if (heads.length > 0) {
+            var node = document.createElement("style");
+            node.type = "text/css";
+            node.appendChild(document.createTextNode(css));
+            heads[0].appendChild(node); 
+        }
+    }
+})();
