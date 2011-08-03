48d159ae194779d91cdc19859842b8c856d7a4d1
Steve Losh <steve@stevelosh.com>
2011-08-03 17:20:40 -0400

    Fix swank.

--- zsh/misc.zsh
+++ zsh/misc.zsh
@@ -75,6 +75,7 @@ function whatthehelldididoon() {
 
 alias dv='dvtm -m "^f"'
 alias dvt='dtach -A /tmp/dvtm-session.sock -r winch dvtm -m "^f"'
+alias swank='dtach -A /tmp/dvtm-swank.sock -r winch lein swank'
 
 alias goawayswapfilesyouareswapfilesidontevenneedyou='rm ~/.vim/tmp/swap/*'
 
