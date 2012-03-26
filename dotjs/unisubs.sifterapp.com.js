6f75f70c1dccc46bd4266c62519264c0035deb6f
Steve Losh <steve@stevelosh.com>
2012-03-26 10:21:27 -0400

    Moar.

--- dotjs/unisubs.sifterapp.com.js
+++ dotjs/unisubs.sifterapp.com.js
@@ -5,4 +5,26 @@ $(function() {
         );
         $(el).remove();
     });
+
+    $('body').keydown(function(e) {
+
+        $tickets = $('tr.issue td.subject a');
+
+        cmds = {
+            '49': function() {
+                document.location = $tickets.eq(0).attr('href');
+            },
+            '50': function() {
+                document.location = $tickets.eq(1).attr('href');
+            },
+            '51': function() {
+                document.location = $tickets.eq(2).attr('href');
+            }
+        };
+
+        if (typeof cmds[e.keyCode] === 'function') {
+            cmds[e.keyCode]();
+        }
+    });
 });
+
