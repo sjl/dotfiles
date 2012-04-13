404f713111aa5caa6b6fd534dcbb8a3367447448
Steve Losh <steve@stevelosh.com>
2012-04-13 15:23:33 -0400

    move
    --HG--
    rename : .ackrc => ackrc
    rename : .ctags => ctags
    rename : .gitconfig => gitconfig
    rename : .hgrc => hgrc
    rename : .offlineimaprc => offlinimaprc
    rename : .pentadactylrc => pentadactylrc
    rename : .urlview => urlview
    rename : vim/.vimrc => vim/vimrc
    rename : .zshrc => zshrc

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -4,6 +4,8 @@
 
 set -e
 
+echo "prerequisites: python git pip dulwich"
+
 function ensure_link {
     test -L "$HOME/$2" || ln -s "$HOME/$1" "$HOME/$2"
 }
@@ -13,8 +15,8 @@ mkdir -p ~/lib/virtualenvs
 mkdir -p ~/bin
 mkdir -p ~/src
 
-test -d ~/.hg-git/        || clone "bb://durin42/hg-git/" "$HOME/.hg-git"
-test -d ~/.hg-git/dulwich || git clone "git://github.com/jelmer/dulwich.git" "$HOME/lib/dulwich"
+test -d ~/.hg-git/    || hg clone "bb://durin42/hg-git/" "$HOME/.hg-git"
+test -d ~/lib/dulwich || git clone "git://github.com/jelmer/dulwich.git" "$HOME/lib/dulwich"
 ensure_link "lib/dulwich" "lib/hg/hg-stable/dulwich"
 
 test -d ~/lib/dotfiles || hg clone http://bitbucket.org/sjl/dotfiles ~/lib/dotfiles
@@ -28,9 +30,14 @@ ensure_link "lib/dotfiles/gitconfig"      ".gitconfig"
 ensure_link "lib/dotfiles/ackrc"          ".ackrc"
 ensure_link "lib/dotfiles/zsh"            "lib/oh-my-zsh/custom"
 ensure_link "lib/dotfiles/zshrc"          ".zshrc"
+ensure_link "lib/dotfiles/weechat"        ".weechat"
+ensure_link "lib/dotfiles/urlview"        ".urlview"
+ensure_link "lib/dotfiles/pentadactylrc"  ".pentadactylrc"
+ensure_link "lib/dotfiles/offlineimaprc"  ".offlineimaprc"
+ensure_link "lib/dotfiles/mutt"           ".mutt"
+ensure_link "lib/dotfiles/dotjs"          ".js"
+ensure_link "lib/dotfiles/dotcss"         ".css"
+ensure_link "lib/dotfiles/hgignore"       ".hgignore"
+ensure_link "lib/dotfiles/ctags"          ".ctags"
 
-hg clone "bb://sjl/hg-prompt/"       "$HOME/lib/dotfiles/mercurial/hg-prompt"
-hg clone "bb://ccaughie/hgcollapse/" "$HOME/lib/dotfiles/mercurial/hgcollapse"
-hg clone "bb://durin42/histedit/"    "$HOME/lib/dotfiles/mercurial/histedit"
-
-
+echo completed
