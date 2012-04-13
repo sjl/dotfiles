7e512ffa5ac06258ad9253216906b3167f524168
Steve Losh <steve@stevelosh.com>
2012-04-13 15:13:59 -0400

    moar.

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -1,28 +1,24 @@
 #!/bin/bash
-
-set -e
-
 # THIS IS A WORK IN PROGRESS
 # BE CAREFUL, DAMMIT
 
-mkdir -p lib/hg
-mkdir -p lib/python
-mkdir -p lib/virtualenvs
-mkdir -p ~/bin
-mkdir -p ~/src
-
-# hg clone 'http://selenic.com/repo/hg#stable' ~/lib/hg/hg-stable
-# cd ~/lib/hg/hg-stable
-# make local
-# cd
-# export PATH="$HOME/lib/hg/hg-stable:$PATH"
-
-test -d ~/lib/dotfiles || hg clone http://bitbucket.org/sjl/dotfiles ~/lib/dotfiles
-test -d ~/lib/oh-my-zsh || git clone git://github.com/sjl/oh-my-zsh ~/lib/oh-my-zsh
+set -e
 
 function ensure_link {
     test -L "$HOME/$2" || ln -s "$HOME/$1" "$HOME/$2"
 }
+
+mkdir -p ~/lib/hg
+mkdir -p ~/lib/virtualenvs
+mkdir -p ~/bin
+mkdir -p ~/src
+
+test -d ~/.hg-git/        || clone "bb://durin42/hg-git/" "$HOME/.hg-git"
+test -d ~/.hg-git/dulwich || git clone "git://github.com/jelmer/dulwich.git" "$HOME/lib/dulwich"
+ensure_link "lib/dulwich" "lib/hg/hg-stable/dulwich"
+
+test -d ~/lib/dotfiles || hg clone http://bitbucket.org/sjl/dotfiles ~/lib/dotfiles
+test -d ~/lib/oh-my-zsh || git clone git://github.com/sjl/oh-my-zsh ~/lib/oh-my-zsh
 
 ensure_link "lib/dotfiles/tmux/tmux.conf" ".tmux.conf"
 ensure_link "lib/dotfiles/vim"            ".vim"
@@ -33,5 +29,8 @@ ensure_link "lib/dotfiles/ackrc"          ".ackrc"
 ensure_link "lib/dotfiles/zsh"            "lib/oh-my-zsh/custom"
 ensure_link "lib/dotfiles/zshrc"          ".zshrc"
 
-git clone "git://github.com/jelmer/dulwich.git" "$HOME/lib/dulwich"
-ensure_link "lib/dulwich" "lib/hg/hg-stable/dulwich"
+hg clone "bb://sjl/hg-prompt/"       "$HOME/lib/dotfiles/mercurial/hg-prompt"
+hg clone "bb://ccaughie/hgcollapse/" "$HOME/lib/dotfiles/mercurial/hgcollapse"
+hg clone "bb://durin42/histedit/"    "$HOME/lib/dotfiles/mercurial/histedit"
+
+
