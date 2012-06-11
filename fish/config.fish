3035745df0eae9192e4443e479dabed51f33d3ef
Steve Losh <steve@stevelosh.com>
2012-06-11 10:05:49 -0400

    Fishify.

--- fish/config.fish
+++ fish/config.fish
@@ -16,10 +16,13 @@ alias vs 'vagrant suspend'
 # }}}
 # Environment variables {{{
 
-set PATH "$HOME/.gem/ruby/1.8/bin:$PATH"
-set PATH "/usr/local/bin:/usr/local/sbin:$HOME/bin:/opt/local/bin:$PATH"
-set PATH "/opt/subversion/bin:$PATH"
-set PATH "$HOME/lib/hg/hg-stable:$PATH"
+set PATH "/usr/local/bin"         $PATH
+set PATH "/usr/local/sbin"        $PATH
+set PATH "$HOME/bin"              $PATH
+set PATH "$HOME/lib/dotfiles/bin" $PATH
+set PATH "/opt/local/bin"         $PATH
+set PATH "/opt/subversion/bin"    $PATH
+set PATH "$HOME/lib/hg/hg-stable" $PATH
 
 set BROWSER open
 
@@ -36,10 +39,10 @@ set -g -x PIP_DOWNLOAD_CACHE "$HOME/.pip/cache"
 set -g -x PYTHONSTARTUP "$HOME/.pythonrc.py"
 set -g -x WORKON_HOME "$HOME/lib/virtualenvs"
 
-set PATH "$PATH:/usr/local/Cellar/PyPi/3.6/bin"
-set PATH "$PATH:/usr/local/Cellar/python/2.7.1/bin"
-set PATH "$PATH:/usr/local/Cellar/python/2.7/bin"
-set PATH "$PATH:/usr/local/Cellar/python/2.6.5/bin"
+set PATH $PATH "/usr/local/Cellar/PyPi/3.6/bin"
+set PATH $PATH "/usr/local/Cellar/python/2.7.1/bin"
+set PATH $PATH "/usr/local/Cellar/python/2.7/bin"
+set PATH $PATH "/usr/local/Cellar/python/2.6.5/bin"
 
 set -g -x PYTHONPATH ""
 set PYTHONPATH "$PYTHONPATH:/usr/local/lib/python2.7.1/site-packages"
