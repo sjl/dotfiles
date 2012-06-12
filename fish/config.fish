27d60871650f20eff2fd291ed3ca4dcb723da440
Steve Losh <steve@stevelosh.com>
2012-06-12 11:56:48 -0400

    moar.

--- fish/config.fish
+++ fish/config.fish
@@ -36,6 +36,8 @@ set PATH "$HOME/lib/hg/hg"         $PATH
 
 set PATH "/usr/local/Cellar/ruby/1.9.3-p194/bin" $PATH
 
+set PATH "/usr/local/Cellar/ruby/1.9.3-p125/bin" $PATH
+
 set BROWSER open
 
 set -g -x fish_greeting ''
@@ -51,6 +53,7 @@ set -g -x PIP_DOWNLOAD_CACHE "$HOME/.pip/cache"
 set -g -x PYTHONSTARTUP "$HOME/.pythonrc.py"
 set -g -x WORKON_HOME "$HOME/lib/virtualenvs"
 
+set PATH $PATH "/usr/local/share/python"
 set PATH $PATH "/usr/local/Cellar/PyPi/3.6/bin"
 set PATH $PATH "/usr/local/Cellar/python/2.7.1/bin"
 set PATH $PATH "/usr/local/Cellar/python/2.7/bin"
